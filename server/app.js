e059722ab41929ab1c30bcbba644ab56dadc1427
B-Payal <b08.payal@gmail.com>
2026-04-13 11:36:16 +0530

    making it connected to frontend

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import cors from 'cors';
 import userRoute from './routes/userRoutes.js';   
 import authRoute from './routes/authRoutes.js'
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 
